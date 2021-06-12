3906436f63a2ade5b38145593f525ba6393b932c
Can202 <mgoopazo@gmail.com>
2021-06-12 02:03:46 -0400

    GUI

--- zerotier.sh
+++ zerotier.sh
@@ -51,6 +51,38 @@ xterm -e "curl -s https://install.zerotier.com | sudo bash"
 
 echo ZeroTier installed
 
+cd /tmp
+
+mkdir ZeroTierInstaller
+
+echo temporaly ZeroTierInstaller Folder created
+
+cd ZeroTierInstaller/
+
+git clone https://github.com/tralph3/ZeroTier-GUI
+
+echo ZeroTier-GUI by tralph3 cloned
+
+
+sudo cp ZeroTier-GUI/src/zerotier-gui /usr/bin/
+
+cd ..
+sudo rm -R ZeroTierInstaller/
+echo ZeroTierInstaller Folder Removed
+
+echo Ready!!!
+
+echo you can run ZeroTier with this command:
+echo
+echo
+echo sudo zerotier-gui
+echo
+echo
+echo now you can close this window
+read nothing
+exit
+
+
 
 
 
